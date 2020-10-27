9e807a507be0512d6da5552c1ca4d743c5ac41ef
Virgil <bugnariu.virgil@gmail.com>
2020-10-27 21:32:21 +0200

    bine

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
 const Furminator = require('./Furminator');
 const hackNSA = require('./MrRobot');
+const jwt = require('jsonwebtoken');
 
+const config = {
+  JWTSECRET: 'SuperSecretKey',
+};
+
+app.use(bodyParser.json());
 app.get('/hello-world', function(req, res) {
   const furminator = new Furminator();
   const body = furminator.getFact();
@@ -23,6 +30,39 @@ app.get('/hackNSA', async function(req, res) {
   });
 });
 
+const authenticationMiddleware = (req, res, next) => {
+  jwt.verify(req.headers.authorization, config.JWTSECRET, (err, data) => {
+    if(err) {
+      res.send({
+        status: "crying_in_the_shower"
+      }, 401);
+    } else {
+      next()
+    }
+  });
+};
+
+
+app.post('/login', (req, res) => {
+  const { user, pass } = req.body;
+  if(user === 'Gogu' && pass === 'P@rOlA') {
+    jwt.sign({}, config.JWTSECRET, (err, token) => {
+      if(!err) {
+        res.send({
+          token,
+        });
+      }
+    })
+  } else {
+    res.send({ error: true }, 401);
+  }
+});
+
+app.get('/graphql', authenticationMiddleware, async function(req, res) {
+  res.send({
+    status: "ok"
+  }, 200);
+});
 
 app.listen(port, function() {
   console.log('server started');

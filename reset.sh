7c6f3a4ef6af7b0576cff09db2c9caa3a2b89c26
Virgil <bugnariu.virgil@gmail.com>
2020-11-25 17:50:10 +0200

    Updated reset script

--- reset.sh
+++ reset.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 rm -f storage/db.sqlite
 touch storage/db.sqlite
 npx sequelize-cli db:migrate
 npx sequelize-cli db:seed:all
-=======
-rm -rf storage/db.sqlite
-touch storage/db.sqlite
-sequelize-cli db:migrate
-sequelize-cli db:seed:all
->>>>>>> adv_db
